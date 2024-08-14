c3063f8e524e3d7c1cac6452c6e976303678b5fd
REALarbazBoi <124275176+REALarbazBoi@users.noreply.github.com>
2024-08-14 06:10:01 +0530

    Add files via upload

---
+++ moduleone.py
@@ -0,0 +1,30 @@
+total_amount = 2000
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+if __name__ == "__main__":
+    print(__name__)
+    print(50)
+    print(60)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
