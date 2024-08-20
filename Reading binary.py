3cd19cbdfa29f11da946f099cf597ba210e2bdbe
REALarbazBoi <124275176+REALarbazBoi@users.noreply.github.com>
2024-08-20 06:24:54 +0530

    Reading binary.py

--- Reading binary.py
+++ Reading binary.py
@@ -1,6 +1,20 @@
+#win1
+f= open("Python1.jpeg","rb")
+
+data = f.read()
+
+cp= open("Python2.jpeg", "wb")
+
+cp.write(data)
+
+f.close()
+cp.close()
+
+#win2
 with open("my.data", "wb") as f:
     f.write(b"abcdefghij")
 
+#win3
 with open("my.data","rb") as f:
     print(f.read(2).decode())
     f.seek(3,0)
