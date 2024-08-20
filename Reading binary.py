58ae146b242ea55a0ff238d207dbbefaae98af8e
REALarbazBoi <124275176+REALarbazBoi@users.noreply.github.com>
2024-08-20 06:18:47 +0530

    Add files via upload

---
+++ Reading binary.py
@@ -0,0 +1,9 @@
+with open("my.data","rb") as f:
+    print(f.read(2).decode())
+    f.seek(3,0)
+    print(f.read(1).decode())
+    f.seek(3,1)
+    print(f.read(1).decode())
+    f.seek(-3,2)
+    print(f.read(1).decode())
+    print(f.tell())
