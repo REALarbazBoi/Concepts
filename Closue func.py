809d6f11aac3c9cbaabad8f2ada1dc1cd85d475d
REALarbazBoi <124275176+REALarbazBoi@users.noreply.github.com>
2024-08-21 06:23:08 +0530

    Closure func.py

--- Closue func.py
+++ Closue func.py
@@ -1,16 +1,16 @@
-# def closure():
-#     msg = "hello"
-#     def display():
-#         print("*"*10)
-#         print(msg)
-#         print(("#"*10))
-#         return 0
-#     return display
-#
-# d = closure()
-# print(d)
-# d()
-#
+def closure():
+    msg = "hello"
+    def display():
+        print("*"*10)
+        print(msg)
+        print(("#"*10))
+        return 0
+    return display
+
+d = closure()
+print(d)
+d()
+
 def closure(msg):
 
     def display():
