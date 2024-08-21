8b8819dd95cb6797de510f5dd2c836cfbeff343e
REALarbazBoi <124275176+REALarbazBoi@users.noreply.github.com>
2024-08-21 06:22:38 +0530

    Add files via upload

---
+++ Closue func.py
@@ -0,0 +1,25 @@
+# def closure():
+#     msg = "hello"
+#     def display():
+#         print("*"*10)
+#         print(msg)
+#         print(("#"*10))
+#         return 0
+#     return display
+#
+# d = closure()
+# print(d)
+# d()
+#
+def closure(msg):
+
+    def display():
+        print("*"*10)
+        print(msg)
+        print(("#"*10))
+        return 0
+    return display
+
+d = closure("Welcome")
+# print(d)
+d()
